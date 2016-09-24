fe5cb94da66d66bf0ab7d760f05f93a21ba5681b
simoneslatnpou <svbyogibear@gmail.com>
2016-09-24 17:54:30 +0200

    Added interface for files.

---
+++ TwitCore/Interfaces/IFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwitCore.Interfaces {
+    public interface IFile { //used for any type of file such as .txt, .pdf, etc.
+        String FileName { get; set; }
+        String Path { get; set; }
+        String Extension { get; set; }
+    }
+}
