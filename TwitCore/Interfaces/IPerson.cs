61764c70860e4f2755e4c41d3cd24fc8823c156c
simoneslatnpou <svbyogibear@gmail.com>
2016-09-24 15:21:32 +0200

    Initial commit to branch master

---
+++ TwitCore/Interfaces/IPerson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwitCore.Interfaces {
+    public interface IPerson { //used to define any person in the twitter-like system
+        String Name { get; set; }
+        String Surname { get; set; }
+    }
+}
