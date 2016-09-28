61d4e327f205057119d7adc487693fa9d8fd5c82
simoneslatnpou <svbyogibear@gmail.com>
2016-09-28 02:42:31 +0200

    Added console application for testing.

---
+++ Twit-Like_Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Twit_Like_Console {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
